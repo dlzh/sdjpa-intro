349f6faf0ae6b8ed2584b727edd25a5b4e877d63
dlzh <dlzhangster@gmail.com>
2023-08-08 19:30:57 +0930

    main abced test

--- src/main/java/guru/springframework/sdjpaintro/SdjpaIntroApplication.java
+++ src/main/java/guru/springframework/sdjpaintro/SdjpaIntroApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SdjpaIntroApplication {
-//DZ updated to test git againab
+//DZ updated to test git againabcde
     public static void main(String[] args) {
         SpringApplication.run(SdjpaIntroApplication.class, args);
     }
