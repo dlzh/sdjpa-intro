6d7cf68f4eacaab13e89196a96d5b0d7f1bd7832
dlzh <dlzhangster@gmail.com>
2023-08-08 18:52:53 +0930

    adfd

--- src/main/java/guru/springframework/sdjpaintro/SdjpaIntroApplication.java
+++ src/main/java/guru/springframework/sdjpaintro/SdjpaIntroApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SdjpaIntroApplication {
-//dz test push
+//dz test push abc
     public static void main(String[] args) {
         SpringApplication.run(SdjpaIntroApplication.class, args);
     }
