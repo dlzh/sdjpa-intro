e79c179067c9f105831a0d342fecb6b7f27d0c4e
dlzh <dlzhangster@gmail.com>
2023-08-08 19:24:13 +0930

    2nd test

--- src/main/java/guru/springframework/sdjpaintro/SdjpaIntroApplication.java
+++ src/main/java/guru/springframework/sdjpaintro/SdjpaIntroApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SdjpaIntroApplication {
-//dz test push abc
+//dz test push abc 2nd time
     public static void main(String[] args) {
         SpringApplication.run(SdjpaIntroApplication.class, args);
     }
